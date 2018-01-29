8518aed0661799635e723bb192a0d0a91e09a507
YinjianChen <YinjianChen02@gmail.com>
2018-01-29 23:55:09 +0800

    add plane detect

--- AR_Jump/ViewController.swift
+++ AR_Jump/ViewController.swift
@@ -39,21 +39,53 @@ class ViewController: UIViewController , ARSCNViewDelegate {
     }
     
    
-    
-    func addGeometric(_ position:SCNVector3,_ type:Int){
-        
+    func createConcrete(planeAnchor: ARPlaneAnchor) -> SCNNode {
+        let concreteNode = SCNNode(geometry: SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(CGFloat(planeAnchor.extent.z))))
+        concreteNode.geometry?.firstMaterial?.diffuse.contents = #imageLiteral(resourceName: "concrete")
+        concreteNode.geometry?.firstMaterial?.isDoubleSided = true
+        concreteNode.position = SCNVector3(planeAnchor.center.x,planeAnchor.center.y,planeAnchor.center.z)
+        concreteNode.eulerAngles = SCNVector3(90.degreesToRadians, 0, 0)
+        let staticBody = SCNPhysicsBody.static()
+        concreteNode.physicsBody = staticBody
+        return concreteNode
     }
-    
     
     //ARSCNViewDelegate
     func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
-        guard anchor is ARPlaneAnchor else {return}
-
+        guard let planeAnchor = anchor as? ARPlaneAnchor else {return}
+        let concreteNode = createConcrete(planeAnchor: planeAnchor)
+        node.addChildNode(concreteNode)
+        print("new flat surface detected, new ARPlaneAnchor added")
     }
 
     func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        guard let planeAnchor = anchor as? ARPlaneAnchor else {return}
+        print("updating floor's anchor...")
+        node.enumerateChildNodes { (childNode, _) in
+            childNode.removeFromParentNode()
+            
+        }
+        let concreteNode = createConcrete(planeAnchor: planeAnchor)
+        node.addChildNode(concreteNode)
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didRemove node: SCNNode, for anchor: ARAnchor) {
+        guard let _ = anchor as? ARPlaneAnchor else {return}
+        node.enumerateChildNodes { (childNode, _) in
+            childNode.removeFromParentNode()
+            
+        }
         
     }
     
 }
 
+func +(left: SCNVector3, right: SCNVector3) -> SCNVector3 {
+    
+    return SCNVector3Make(left.x + right.x, left.y + right.y, left.z + right.z)
+    
+}
+extension Int {
+    
+    var degreesToRadians: Double { return Double(self) * .pi/180}
+}
