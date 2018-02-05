54a628e6249ce04a4746ff3384de8b78e61b9e28
YinjianChen <YinjianChen02@gmail.com>
2018-02-05 23:16:06 +0800

    remove detect

--- AR_Jump/ViewController.swift
+++ AR_Jump/ViewController.swift
@@ -19,59 +19,102 @@ class ViewController: UIViewController , ARSCNViewDelegate,ARSessionDelegate,SCN
 
     @IBOutlet weak var arscnView: ARSCNView!
     let configuration = ARWorldTrackingConfiguration()
-    var power: Float = 1
-    let timer = Each(0.05).seconds
-    var targetOren : SCNVector4!
+    var power: Float = 50
+//    let timer = Each(0.05).seconds
     var Target: SCNNode?
-    var value:Int!
     var didAddTarget:Bool!
     override func viewDidLoad() {
         super.viewDidLoad()
         self.arscnView.debugOptions = [ ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
-        if #available(iOS 11.3, *) {
-            self.configuration.planeDetection = .vertical
-        } else {
-            // Fallback on earlier versions
-            self.configuration.planeDetection = .horizontal
-
-        }
+//        if #available(iOS 11.3, *) {
+//            self.configuration.planeDetection = .vertical
+//        } else {
+//            // Fallback on earlier versions
+//            self.configuration.planeDetection = .horizontal
+//
+//        }
         self.arscnView.session.run(configuration)
+        self.arscnView.autoenablesDefaultLighting = true
+
         self.arscnView.delegate = self
         self.arscnView.session.delegate = self
         self.arscnView.scene.physicsWorld.contactDelegate = self
-        
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleTap(sender:)))
+        self.arscnView.addGestureRecognizer(gestureRecognizer)
         didAddTarget = false
+
 
     }
 
     func createTarget(planeAnchor: ARPlaneAnchor) -> SCNNode {
 
         let targetScene = SCNScene(named: "Jump.scnassets/Target.scn")
-  
         let targetNode = (targetScene?.rootNode.childNode(withName: "target", recursively: false))!
         targetNode.position = SCNVector3(planeAnchor.center.x,planeAnchor.center.y,planeAnchor.center.z)
+        targetNode.physicsBody = SCNPhysicsBody(type: .static, shape: SCNPhysicsShape(node: targetNode, options: nil))
         targetNode.eulerAngles = SCNVector3(270.degreesToRadians, 0, 0)
-
-        let staticBody = SCNPhysicsBody.static()
-        targetNode.physicsBody = staticBody
-        targetNode.physicsBody?.categoryBitMask = 2|3|4|5|6|7|8|9|10|11
+        targetNode.physicsBody?.categoryBitMask = 12
         targetNode.physicsBody?.contactTestBitMask = 1
+    
+
+        let scoreTen = targetNode.childNode(withName: "10", recursively: false)
+        let scoreNight = targetNode.childNode(withName: "9", recursively: false)
+        scoreTen?.physicsBody = SCNPhysicsBody(type: .static, shape: SCNPhysicsShape(node: scoreTen!, options: nil))
+        scoreTen?.physicsBody?.categoryBitMask = 10
+        scoreTen?.physicsBody?.contactTestBitMask = 1
+
         return targetNode
     }
     
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        timer.perform(closure: { () -> NextStep in
-            self.power = self.power + 1
-            return .continue
-        })
+    @objc func handleTap(sender: UITapGestureRecognizer) {
+        if(!didAddTarget){
+            let targetScene = SCNScene(named: "Jump.scnassets/tar.scn")
+            let targetNode = (targetScene?.rootNode.childNode(withName: "tar", recursively: false))!
+            targetNode.position = SCNVector3(0,0,-0.02)
+            targetNode.physicsBody = SCNPhysicsBody(type: .static, shape: SCNPhysicsShape(node: targetNode, options: nil))
+            targetNode.scale = SCNVector3(0.02,0.02,0.02)
+//            targetNode.physicsBody?.categoryBitMask = 12
+//            targetNode.physicsBody?.contactTestBitMask = 1
+            self.arscnView.scene.rootNode.addChildNode(targetNode)
+
+        }else{
+            guard let sceneView = sender.view as? ARSCNView else {return}
+            guard let pointOfView = sceneView.pointOfView else {return}
+            let transform = pointOfView.transform
+            let orientation = SCNVector3(-transform.m31, -transform.m32, -transform.m33)
+            let location = SCNVector3(transform.m41, transform.m42, transform.m43)
+            let position = orientation + location
+            let bullet = SCNNode(geometry: SCNSphere(radius: 0.1))
+            bullet.geometry?.firstMaterial?.diffuse.contents = UIColor.red
+            bullet.position = position
+            let body = SCNPhysicsBody(type: .dynamic, shape: SCNPhysicsShape(node: bullet, options: nil))
+            body.isAffectedByGravity = false
+            bullet.physicsBody = body
+            bullet.physicsBody?.applyForce(SCNVector3(orientation.x*power, orientation.y*power, orientation.z*power), asImpulse: true)
+            bullet.physicsBody?.categoryBitMask = 1
+            bullet.physicsBody?.contactTestBitMask = 12
+            self.arscnView.scene.rootNode.addChildNode(bullet)
+            bullet.runAction(
+                SCNAction.sequence([SCNAction.wait(duration: 2.0),
+                                    SCNAction.removeFromParentNode()])
+            )
+        }
+        
     }
     
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        self.timer.stop()
-        self.shootBullet()
-        self.power = 1
-
-    }
+    
+//    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        timer.perform(closure: { () -> NextStep in
+//            self.power = self.power + 1
+//            return .continue
+//        })
+//    }
+//
+//    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        self.timer.stop()
+//        self.shootBullet()
+//        self.power = 1
+//    }
     
     func shootBullet(){
         guard let pointOfView = self.arscnView.pointOfView else {return}
@@ -89,7 +132,7 @@ class ViewController: UIViewController , ARSCNViewDelegate,ARSessionDelegate,SCN
         bullet.physicsBody = body
         bullet.physicsBody?.applyForce(SCNVector3(orientation.x*power, orientation.y*power, orientation.z*power), asImpulse: true)
         bullet.physicsBody?.categoryBitMask = 1
-        bullet.physicsBody?.contactTestBitMask = 2|3|4|5|6|7|8|9|10|11
+        bullet.physicsBody?.contactTestBitMask = 12
         self.arscnView.scene.rootNode.addChildNode(bullet)
         bullet.runAction(
             SCNAction.sequence([SCNAction.wait(duration: 2.0),
@@ -98,46 +141,20 @@ class ViewController: UIViewController , ARSCNViewDelegate,ARSessionDelegate,SCN
     }
     
     
-    //ARSCNViewDelegate
-    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
-        guard let planeAnchor = anchor as? ARPlaneAnchor else {return}
-        if(didAddTarget == false){
-            let targetNode = createTarget(planeAnchor: planeAnchor)
-            node.addChildNode(targetNode)
-            didAddTarget = true
-        }
-  
-        print("new flat surface detected, new ARPlaneAnchor added")
-    }
-
-    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
-        guard let planeAnchor = anchor as? ARPlaneAnchor else {return}
-        print("updating floor's anchor...")
-
-        if(didAddTarget == false){
-            let targetNode = createTarget(planeAnchor: planeAnchor)
-            node.addChildNode(targetNode)
-            didAddTarget = true
-        }
-    }
-    
-    func renderer(_ renderer: SCNSceneRenderer, didRemove node: SCNNode, for anchor: ARAnchor) {
-        guard let _ = anchor as? ARPlaneAnchor else {return}
-        
-    }
+ 
     
     func physicsWorld(_ world: SCNPhysicsWorld, didBegin contact: SCNPhysicsContact) {
-        let nodeA = contact.nodeA
-        let nodeB = contact.nodeB
-        if nodeA.physicsBody?.categoryBitMask == 2|3|4|5|6|7|8|9|10|11 {
-            self.Target = nodeA
-        } else if nodeB.physicsBody?.categoryBitMask == 2|3|4|5|6|7|8|9|10|11 {
-            self.Target = nodeB
-        }
-
-        Target?.removeFromParentNode()
-
-        return
+//        let nodeA = contact.nodeA
+//        let nodeB = contact.nodeB
+//        if nodeA.physicsBody?.categoryBitMask == 10 {
+//            self.Target = nodeA
+//        } else if nodeB.physicsBody?.categoryBitMask == 10 {
+//            self.Target = nodeB
+//        }
+//
+//        Target?.removeFromParentNode()
+        print("----------------")
+    //    return
         
     }
 
