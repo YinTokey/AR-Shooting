5854f4f224d404d29b09ad8809f15ab918455123
YinjianChen <YinjianChen02@gmail.com>
2018-02-07 00:26:21 +0800

    some adjust

--- AR_Jump/ViewController.swift
+++ AR_Jump/ViewController.swift
@@ -8,23 +8,16 @@
 
 import UIKit
 import ARKit
-import Each
-
-//enum BitMaskCategory: Int {
-//    case bullet = 1
-//    case target = 4
-//}
 
 class ViewController: UIViewController , ARSCNViewDelegate,ARSessionDelegate,SCNPhysicsContactDelegate {
 
     @IBOutlet weak var arscnView: ARSCNView!
     let configuration = ARWorldTrackingConfiguration()
     var power: Float = 50
-    let timer = Each(0.05).seconds
+ 
     var Target: SCNNode?
     var didAddTarget:Bool!
     var addButton:UIButton!
-    var targetsArray:Array<Any>!
     var distance:Double = 0.0
     var scoreLabel:UILabel!
     var totalScore:Int = 0
@@ -62,18 +55,18 @@ class ViewController: UIViewController , ARSCNViewDelegate,ARSessionDelegate,SCN
         if(!didAddTarget){
             let targetScene = SCNScene(named: "Jump.scnassets/tar.scn")
             let targetNode = (targetScene?.rootNode.childNode(withName: "tar", recursively: false))!
-            targetNode.position = SCNVector3(0,0,-6)
+            targetNode.position = SCNVector3(0,0,-5)
             targetNode.physicsBody = SCNPhysicsBody(type: .static, shape: SCNPhysicsShape(node: targetNode, options: nil))
-            targetNode.scale = SCNVector3(1.5,1.5,1.5)
+            targetNode.scale = SCNVector3(1.9,1.9,1.9)
             targetNode.physicsBody?.categoryBitMask = 12
             targetNode.physicsBody?.contactTestBitMask = 1
-//            self.arscnView.scene.rootNode.addChildNode(targetNode)
+            self.arscnView.scene.rootNode.addChildNode(targetNode)
             didAddTarget = true
             addButton.isHidden = true
             
-            for i in 1...10{
-                self.addSmallTarget(i, targetNode)
-            }
+//            for i in 1...10{
+//                self.addSmallTarget(i, targetNode)
+//            }
 
         }
     }
@@ -102,14 +95,15 @@ class ViewController: UIViewController , ARSCNViewDelegate,ARSessionDelegate,SCN
         let location = SCNVector3(transform.m41, transform.m42, transform.m43)
         let position = orientation + location
         let bullet = SCNNode(geometry: SCNSphere(radius: 0.03))
-        bullet.geometry?.firstMaterial?.diffuse.contents = UIColor.red
+        bullet.geometry?.firstMaterial?.diffuse.contents = UIColor.purple
+        bullet.geometry?.firstMaterial?.lightingModel = SCNMaterial.LightingModel.blinn
         bullet.position = position
         let body = SCNPhysicsBody(type: .dynamic, shape: SCNPhysicsShape(node: bullet, options: nil))
         body.isAffectedByGravity = false
         bullet.physicsBody = body
         bullet.physicsBody?.applyForce(SCNVector3(orientation.x*power, orientation.y*power, orientation.z*power), asImpulse: true)
         bullet.physicsBody?.categoryBitMask = 1
-        bullet.physicsBody?.contactTestBitMask = 2|3|4|5|6|7|8|9|10|11
+        bullet.physicsBody?.contactTestBitMask = 12
         self.arscnView.scene.rootNode.addChildNode(bullet)
         bullet.name = "bullet"
         bullet.runAction(
@@ -117,7 +111,7 @@ class ViewController: UIViewController , ARSCNViewDelegate,ARSessionDelegate,SCN
                                 SCNAction.removeFromParentNode()])
         )
         
-        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 2, execute: {
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 1, execute: {
             if(self.distance > 0){
                 self.calculateScore()
                 self.distance = 0
@@ -159,54 +153,7 @@ class ViewController: UIViewController , ARSCNViewDelegate,ARSessionDelegate,SCN
         }
         self.totalScore += score
     }
-    
-//    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-//        timer.perform(closure: { () -> NextStep in
-//            self.power = self.power + 1
-//            return .continue
-//        })
-//    }
-//
-//    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-//        self.timer.stop()
-//        self.shootDarts()
-//        self.power = 1
-//    }
-    
-    func shootDarts(){
-        self.removeEveryOtherDarts()
 
-        let dartScene = SCNScene(named: "Jump.scnassets/darts.scn")
-        let dartNode = (dartScene?.rootNode.childNode(withName: "darts", recursively: false))!
-        guard let pointOfView = self.arscnView.pointOfView else {return}
-        let transform = pointOfView.transform
-        let location = SCNVector3(transform.m41, transform.m42, transform.m43)
-        let orientation = SCNVector3(-transform.m31, -transform.m32, -transform.m33)
-        let position = location + orientation
-        dartNode.position = position
-        dartNode.scale = SCNVector3(0.07,0.07,0.07)
-        let body = SCNPhysicsBody(type: .dynamic, shape: SCNPhysicsShape(node: dartNode))
-        body.isAffectedByGravity = false
-        body.restitution = 0.2
-        dartNode.name = "darts"
-        dartNode.physicsBody = body
-        dartNode.physicsBody?.applyForce(SCNVector3(orientation.x*power * 0.2, orientation.y*power * 0.2, orientation.z*power * 0.2), asImpulse: true)
-        dartNode.physicsBody?.categoryBitMask = 1
-        dartNode.physicsBody?.contactTestBitMask = 2|3|4|5|6|7|8|9|10|11
-        self.arscnView.scene.rootNode.addChildNode(dartNode)
-        dartNode.runAction(
-            SCNAction.sequence([SCNAction.wait(duration: 5.0),
-                                SCNAction.removeFromParentNode()])
-        )
-    }
-    
-    func removeEveryOtherDarts() {
-        self.arscnView.scene.rootNode.enumerateChildNodes { (node, _) in
-            if node.name == "darts" {
-                node.removeFromParentNode()
-            }
-        }
-    }
     
     func removebullet() {
         self.arscnView.scene.rootNode.enumerateChildNodes { (node, _) in
@@ -217,9 +164,9 @@ class ViewController: UIViewController , ARSCNViewDelegate,ARSessionDelegate,SCN
     }
     
     func physicsWorld(_ world: SCNPhysicsWorld, didBegin contact: SCNPhysicsContact) {
-
-        print("------point  \(contact.contactPoint)")
         let point:SCNVector3 = contact.contactPoint
+        print("----- \(point)")
+
         self.distance = sqrt(Double(point.x * point.x + point.y * point.y))
 
         self.removebullet()
