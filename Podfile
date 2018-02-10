fd8e405d28a24ef61b1dc78f443cecded94f63bd
YinjianChen <YinjianChen02@gmail.com>
2018-02-10 22:25:04 +0800

    remove each

--- Podfile
+++ Podfile
@@ -1,5 +1,5 @@
 target 'AR_Shooting_target' do
 use_frameworks!
-pod 'Each', '~> 1.2'
+
 
 end
--- Pods/Target Support Files/Each/Each-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Each : NSObject
-@end
-@implementation PodsDummy_Each
-@end
